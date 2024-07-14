f3b6da924770bfc9ccc47ba0007e0e0f896e8094
AvazxonAD <asqarovavazxon77@gmail.com>
2024-07-14 11:21:15 +0500

    milliy

--- controller/task.controller.js
+++ controller/task.controller.js
@@ -34,3 +34,35 @@ exports.getAllTasks = asyncHandler(async (req, res, next) => {
         data: result
     })
 })
+
+// filter by status 
+exports.filterByStatus = asyncHandler(async (req, res, next) => {
+    let tasks = null
+    if(req.query.inProgress){
+        tasks = await pool.query(`SELECT id, contractnumber, clientname, workernumber, taskdate, tasktime, inProgress, done, notdone 
+            FROM tasks 
+            WHERE user_id = $1 AND inprogress = $2`, [req.user.id, true])
+    }
+
+    else if(req.query.done){
+        tasks = await pool.query(`SELECT id, contractnumber, clientname, workernumber, taskdate, tasktime, inProgress, done, notdone 
+            FROM tasks 
+            WHERE user_id = $1 AND done = $2`, [req.user.id, true])
+    }
+
+    else if(req.query.notDone){
+        tasks = await pool.query(`SELECT id, contractnumber, clientname, workernumber, taskdate, tasktime, inProgress, done, notdone 
+            FROM tasks 
+            WHERE user_id = $1 AND notdone = $2`, [req.user.id, true])
+    }
+
+    let result = tasks.rows.map(task => {
+        task.taskdate = returnStringDate(task.taskdate)
+        return task
+    })
+    return res.status(200).json({
+        success: true,
+        data: result
+    })
+
+})
--- router/task.router.js
+++ router/task.router.js
@@ -5,8 +5,10 @@ const protect = require('../middleware/auth')
 
 const {
     getAllTasks,
+    filterByStatus
 } = require('../controller/task.controller')
 
 router.get('/get/tasks/', protect, getAllTasks)
+router.get('/filter/by/status', protect, filterByStatus)
 
 module.exports = router
