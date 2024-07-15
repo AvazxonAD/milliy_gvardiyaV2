061f28e861c9cbdef97537c5c5f9f061ed983166
AvazxonAD <asqarovavazxon77@gmail.com>
2024-07-15 11:08:02 +0500

    milliy

--- controller/task.controller.js
+++ controller/task.controller.js
@@ -39,19 +39,19 @@ exports.getAllTasks = asyncHandler(async (req, res, next) => {
 exports.filterByStatus = asyncHandler(async (req, res, next) => {
     let tasks = null
     if (req.query.inProgress) {
-        tasks = await pool.query(`SELECT id, contractnumber, clientname, workernumber, taskdate, tasktime, inProgress, done, notdone 
+        tasks = await pool.query(`SELECT id, contractnumber, clientname, workernumber, taskdate, tasktime, inProgress, done, notdone, addres
             FROM tasks 
             WHERE user_id = $1 AND inprogress = $2`, [req.user.id, true])
     }
 
     else if (req.query.done) {
-        tasks = await pool.query(`SELECT id, contractnumber, clientname, workernumber, taskdate, tasktime, inProgress, done, notdone 
+        tasks = await pool.query(`SELECT id, contractnumber, clientname, workernumber, taskdate, tasktime, inProgress, done, notdone, address
             FROM tasks 
             WHERE user_id = $1 AND done = $2`, [req.user.id, true])
     }
 
     else if (req.query.notDone) {
-        tasks = await pool.query(`SELECT id, contractnumber, clientname, workernumber, taskdate, tasktime, inProgress, done, notdone 
+        tasks = await pool.query(`SELECT id, contractnumber, clientname, workernumber, taskdate, tasktime, inProgress, done, notdone, address
             FROM tasks 
             WHERE user_id = $1 AND notdone = $2`, [req.user.id, true])
     }
