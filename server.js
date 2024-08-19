650ce6ff46660c914bbb85a3261544593438eb5d
AvazxonAD <asqarovavazxon77@gmail.com>
2024-08-19 20:59:13 +0500

    milliy

--- server.js
+++ server.js
@@ -8,9 +8,8 @@ app.use(express.urlencoded({extended: false}))
 
 require('dotenv').config()
 require('colors')
-app.use(cors({
-    origin: 'http://147.45.107.174:3000',
-  }))
+
+app.use(cors())
 
 require('./utils/createAdmin')()
 
